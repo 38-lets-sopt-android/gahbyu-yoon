98f2f30012e3e61ef676edc0fb3a1613fefbb8d3
gahbyu <gahbyu@inha.edu>
2026-04-29 22:26:35 +0900

    fix: 오류 수정

--- app/src/main/java/com/example/letssopt/navigation/BottomNavItem.kt
+++ app/src/main/java/com/example/letssopt/navigation/BottomNavItem.kt
@@ -8,10 +8,10 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.ui.graphics.vector.ImageVector
 
-sealed class `BottomNavItem`(val route: String, val label: String, val icon: ImageVector) {
-    object Main : `BottomNavItem`("main", "메인", Icons.Default.Home)
-    object Purchase : `BottomNavItem`("purchase", "개별구매", Icons.Default.ShoppingCart)
-    object Webtoon : `BottomNavItem`("webtoon", "웹툰", Icons.Default.List)
-    object Search : `BottomNavItem`("search", "찾기", Icons.Default.Search)
-    object Library : `BottomNavItem`("library", "보관함", Icons.Default.Folder)
+sealed class BottomNavItem(val route: String, val label: String, val icon: ImageVector) {
+    object Main : BottomNavItem("main", "메인", Icons.Default.Home)
+    object Purchase : BottomNavItem("purchase", "개별구매", Icons.Default.ShoppingCart)
+    object Webtoon : BottomNavItem("webtoon", "웹툰", Icons.Default.List)
+    object Search : BottomNavItem("search", "찾기", Icons.Default.Search)
+    object Library : BottomNavItem("library", "보관함", Icons.Default.Folder)
 }
